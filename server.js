96aa44a5df667b45d9bfb0e9e8d11fd4d7aa710d
Piotr Karpala <pkarpala@infusion.com>
2018-08-17 13:54:14 -0400

    tokens
    Signed-off-by: Piotr Karpala <pkarpala@infusion.com>

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ http.createServer(function(request, response) {
   if(uri === '/api/version') {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Version: 1.1.1.1\n");
+    response.write("Build: #{Build.BuildId}#\n");
     response.end();
     return;
   }
@@ -51,7 +52,7 @@ http.createServer(function(request, response) {
     if (fs.statSync(filename).isDirectory()) filename += '/index.html';
 
     fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
+      if(err) {
         response.writeHead(500, {"Content-Type": "text/plain"});
         response.write(err + "\n");
         response.end();
