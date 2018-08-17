fa92ec8b11776bf2aa2644fa1ff362b381a55f07
Jonathon Powell <jonathon.w.powell@avanade.com>
2018-08-17 11:00:05 -0400

    Improved web server

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 'use strict';
-var http = require('http');
-var port = process.env.PORT || 8080;
-var fs = require('fs');
 const appInsights = require('applicationinsights');
 appInsights.setup();
 appInsights.start();
 
-http.createServer(function (req, res) {
-    fs.readFile('index.html', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
-        res.write(data);
-        res.end();
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    port = process.argv[2] || 8888;
+
+http.createServer(function(request, response) {
+
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
+
+  fs.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {        
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
     });
-}).listen(port);
+  });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
